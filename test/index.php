81823a10a4dd518f06f9a38efd75897c72c22d3c
emili <emilio.borges2012@gmail.com>
2019-09-10 01:36:11 -0300

    Alterado

--- test/index.php
+++ test/index.php
@@ -3,7 +3,7 @@ require __DIR__ . '/../lib_ext/autoload.php';
 
 use Notification\Email;
 
-$novoEmail = new Email("2", "smtp.mail.yahoo.com", "emilio.borges2012@yahoo.com.br", "f6d4m1c4", "ssl", '465', "emilio.borges2012@yahoo.com.br", "emilio");
+$novoEmail = new Email("2", "smtp.mail.yahoo.com", "emilio.borges2012@yahoo.com.br", "123546", "ssl", '465', "emilio.borges2012@yahoo.com.br", "emilio");
 $novoEmail->sendMail("Assunto de teste", "<p>Esse é um e-mail de <b>teste</b>:</p>",
     "emilio.borges2012@yahoo.com.br", "emilio", "emilio.borges2012@gmail.com", "Borges");
 
